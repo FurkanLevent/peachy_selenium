5f138d713a6d5b0f79c9f315ccaa8a19f9b246cf
Furkan Levent Oturaklıoğlu <furkanleventoturaklioglu@gmail.com>
2019-10-16 23:06:57 +0300

    added pom.xml

---
+++ Expedia/src/main/java/Test_Clases/TestNG_Search.java
@@ -0,0 +1,71 @@
+package Test_Clases;
+
+import Pages.FlightSearchPage;
+import Pages.HotelSearchPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class TestNG_Search {
+
+    private WebDriver driver;
+    private String baseURL;
+
+
+    @BeforeMethod
+    public void setup(){
+       System.setProperty("webdriver.chrome.driver", "/Users/furkanleventoturaklioglu/Documents/Selenium/Expedia/src/Drivers/chromedriver");
+        baseURL= "https://www.expedia.com";
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    public void TestFlyTestCase(){
+        driver.get(baseURL);
+        FlightSearchPage.FlightTabClick(driver).click();
+        FlightSearchPage.FlyingFrom(driver).sendKeys("Istanbul");
+        FlightSearchPage.FlyingTo(driver).sendKeys("Ankara");
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        FlightSearchPage.FlyDeptDate(driver).sendKeys("12/08/2019");
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        FlightSearchPage.FlyReturnDate(driver).sendKeys("08/30/2020");
+        FlightSearchPage.FlyTravelers(driver).click();
+        FlightSearchPage.FlyTravelersAdult(driver).click();
+        FlightSearchPage.FlyTravelersChildren(driver).click();
+        FlightSearchPage.FlyClickSearch(driver).click();
+
+    }
+
+    	@Test
+        public void TestHotelCase(){
+		driver.get(baseURL);
+            HotelSearchPage.HotelTabcClick(driver).click();
+            HotelSearchPage.getGoingTo(driver).sendKeys("Ankara");
+            HotelSearchPage.getCheckIn(driver).sendKeys("12/08/2019");
+            driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
+            HotelSearchPage.getCheckOut(driver).sendKeys("08/30/2020");
+            driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+            HotelSearchPage.HotelTravelers(driver).click();
+            driver.manage().timeouts().implicitlyWait(3,TimeUnit.SECONDS);
+            HotelSearchPage.HotelTravelersAdult(driver).click();
+            HotelSearchPage.HotelSearchbutton(driver).click();
+
+        }
+
+
+    @AfterMethod
+    public void afterMethod() {
+    }
+
+
+
+
+}
